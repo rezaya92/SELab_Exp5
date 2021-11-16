1663395ec323fb826fd539fbcee4e57f7fdcc5a0
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-11-16 18:14:32 +0330

    add quicksort

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
 import java.util.Random;
 
 public class Main {
+    static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for(int j = low; j <= high - 1; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
     static void bubbleSort(int[] arr)
     {
         int n = arr.length;
@@ -40,7 +73,8 @@ public class Main {
         int[] arr = randomArray();
         System.out.println("original array");
         printArray(arr);
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        quickSort(arr, 0, arr.length - 1);
         System.out.println("sorted array");
         printArray(arr);
     }
